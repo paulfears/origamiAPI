b719d08630757f034e67534f84061cc40447795f
Paul Fears <paulrfears@gmail.com>
2023-10-09 19:28:33 -0500

    setup

---
+++ netlify/functions/test.ts
@@ -0,0 +1,8 @@
+import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  // your server-side functionality
+  console.log("here");
+};
+
+export { handler };
