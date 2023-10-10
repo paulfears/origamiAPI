a8d227ea65b7e2b6069180e906c235aa9b20be6a
Paul Fears <paulrfears@gmail.com>
2023-10-09 19:35:28 -0500

    function update

--- netlify/functions/test.ts
+++ netlify/functions/test.ts
@@ -3,7 +3,16 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   // your server-side functionality
   console.log("here");
-  return {"hello":"world"}
+  const json_data = {
+    "hello":"world"
+  }
+    return {'statusCode': 200,
+        'headers': {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": 'GET, POST, PUT, DELETE, OPTIONS'
+        },
+        'body': json_data
+    }    
 };
 
 export { handler };
