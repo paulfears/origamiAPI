5f89266b2660a71e89e853a6d884028a582b3be3
Paul Fears <paulrfears@gmail.com>
2023-10-09 23:00:47 -0500

    changed referer polcy

--- netlify/functions/createaccount.ts
+++ netlify/functions/createaccount.ts
@@ -13,8 +13,7 @@ async function postData(url, data = {}) {
         "Authorization": (process.env.STELLARIDAPIKEY as string),
         "accept": "application/json"
       },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      redirect: "follow", // manual, *follow, error// no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data), // body data type must match "Content-Type" header
     });
     console.log("response.ok is: ");
