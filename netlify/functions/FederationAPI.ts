66e57ae9a2a00e5ad790a40d7b40a3a1731aed93
Paul Fears <paulrfears@gmail.com>
2023-10-09 19:45:51 -0500

    post additions

--- netlify/functions/FederationAPI.ts
+++ netlify/functions/FederationAPI.ts
@@ -1,5 +1,10 @@
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     // your server-side functionality
+    console.log("event is ");
+    console.log(event);
+    console.log("context is ");
+    console.log(context);
+    console.log("event body is");
     console.log(event.body);
     console.log("here");
     const json_data = {
--- netlify/functions/test.ts
+++ netlify/functions/test.ts
@@ -1,18 +1,23 @@
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  // your server-side functionality
-  console.log("here");
-  const json_data = {
-    "hello":"world"
-  }
-    return {'statusCode': 200,
-        'headers': {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": 'GET, POST, PUT, DELETE, OPTIONS'
-        },
-        'body': JSON.stringify(json_data)
-    }    
-};
-
-export { handler };
+    // your server-side functionality
+    console.log("event is ");
+    console.log(event);
+    console.log("context is ");
+    console.log(context);
+    console.log("event body is");
+    console.log(event.body);
+    console.log("here");
+    const json_data = {
+      "hello":"world",
+      "postData": event.body
+    }
+      return {'statusCode': 200,
+          'headers': {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": 'GET, POST, PUT, DELETE, OPTIONS'
+          },
+          'body': JSON.stringify(json_data)
+      }    
+  };
+  
+  export { handler };
