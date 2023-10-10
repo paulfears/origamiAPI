58bef77d9ca50eac9fa3b2132863bedaaeb8c94e
Paul Fears <paulrfears@gmail.com>
2023-10-09 19:32:16 -0500

    return value on test

--- netlify/functions/test.ts
+++ netlify/functions/test.ts
@@ -3,6 +3,7 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   // your server-side functionality
   console.log("here");
+  return "here"
 };
 
 export { handler };
