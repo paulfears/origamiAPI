968dee78b511d1a3c51a7ab6dda8c626b0acbf23
Paul Fears <paulrfears@gmail.com>
2023-10-09 22:24:33 -0500

    with cors now ig

--- netlify/functions/createaccount.ts
+++ netlify/functions/createaccount.ts
@@ -5,7 +5,7 @@ async function postData(url, data = {}) {
 
     const response = await fetch(url, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "no-cors", // no-cors, *cors, same-origin
+      mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       headers: {
         "Content-Type": "application/json",
