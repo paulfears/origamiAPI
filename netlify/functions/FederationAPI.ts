0f62b9f23f51f041b0443ab53b5b39f24cae29d1
Paul Fears <paulrfears@gmail.com>
2023-10-09 19:39:14 -0500

    post body

---
+++ netlify/functions/FederationAPI.ts
@@ -0,0 +1,18 @@
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    // your server-side functionality
+    console.log(event.body);
+    console.log("here");
+    const json_data = {
+      "hello":"world",
+      "postData": event.body
+    }
+      return {'statusCode': 200,
+          'headers': {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": 'GET, POST, PUT, DELETE, OPTIONS'
+          },
+          'body': JSON.stringify(json_data)
+      }    
+  };
+  
+  export { handler };
